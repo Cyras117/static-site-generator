34c10baf1e86b0447102f5375782dd9692804f49
Cyras117 <alexsandro.oliveira117@gmail.com>
2024-07-15 22:30:33 -0300

    adding transferFiles

--- src/main.py
+++ src/main.py
@@ -3,50 +3,23 @@ from htmlnode import HTMLnode
 from leafnode import LeafNode
 from parentnode import ParentNode
 import utils as u
+import os,shutil
 
+def transferFiles(src,dst ,clear_folder = False):
+    if clear_folder:
+        shutil.rmtree(dst)
+        os.mkdir(dst)
+    
+    for item in os.listdir(src):
+        item_path = os.path.join("/",src,item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path,dst)
+        if os.path.isdir(item_path):
+            os.mkdir(os.path.join("/",dst,item))
+            transferFiles(os.path.join("/",src,item),os.path.join("/",dst,item))
+        print(item_path)
+   
 def main():
-    # node = ParentNode(
-    # "p",
-    # [
-    #     LeafNode("b", "Bold text"),
-    #     LeafNode(None, "Normal text"),
-    #     LeafNode("i", "italic text"),
-    #     LeafNode(None, "Normal text"),
-    #     ParentNode("p2",[
-    #         LeafNode("i2", "italic text"),
-    #         LeafNode(None, "Normal text2"),
-    #     ],{"test":"props"}),
-    # ],{"test0":"props0"},
-    # )
-    # print(node.to_html())
-    # n = [TextNode("This is text with a `code block` word", "text"),TextNode("This is text with a `code block` word2", "text2")]
-    # b = split_nodes_delimiter(n,"`","code")
-
-    # print(TextNode("This is a text node", "bold", "https://www.boot.dev"))
-    # print(HTMLnode(None,None,[],{"href": "https://www.google.com"}))
-    # print(LeafNode("p", "This is a paragraph of text.").render())
-    # print(LeafNode("a", "Click me!", {"href": "https://www.google.com"}).render())
-    # print(LeafNode.text_node_to_html_node(TextNode("This is a text node", "text", "https://www.boot.dev")).render())
-    #print(extract_markdown_images("This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"))
-    #print(extract_markdown_links("This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"))
-    # tn = TextNode(
-    #     "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
-    #     "text",
-    # )
-    # tn2 = TextNode(
-    #     "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)",
-    #     "text",
-    # )
-    # print(split_nodes_images([tn]))
-    # print(extract_markdown_images(tn.text))
-    #print(split_nodes_link([tn2]))
-    # txt = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
-    # print(text_to_textnodes(txt))
-    # list_txt = [TextNode("This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)", "text")]
-    # print(split_nodes_delimiter(list_txt,"**","bold"))
-    # print(split_nodes_delimiter(list_txt,"*","italic"))
-
-    markdown = "This is **bolded** paragraph\n\nThis is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line\n\n* This is a list\n* with items\n"
-    print(u.markdown_to_blocks(markdown))
+    transferFiles(os.getcwd()+"/static",os.getcwd()+"/public",True)
 
 main()
